5ea3eb2b1a7d827a57e15a2e779fe83d7122463d
Mahinthan <mahinthan@buzzcar.com>
2014-01-13 09:18:42 +0100

    started working on css

--- js/script.js
+++ js/script.js
@@ -8,12 +8,29 @@ $(document).ready(function(){
              
 		})
 	});
+
+
 		
 });
 
 /*	
 		$( ".ui-panel-inner" ).append( "<ul data-role='listview' class='ui-listview'><li>" + val.firstname + " " + val.lastname + "<li></ul>" );
 */
+
+// $( document ).on( "pageinit", "#page", function() {
+//     $( document ).on( "swipeleft swiperight", "#page", function( e ) {
+//         // We check if there is no open panel on the page because otherwise
+//         // a swipe to close the left panel would also open the right panel (and v.v.).
+//         // We do this by checking the data that the framework stores on the page element (panel: open).
+//         if ( $.mobile.activePage.jqmData( "panel" ) !== "open" ) {
+//             if ( e.type === "swipeleft"  ) {
+//                 $( "#mypanel" ).panel( "close" );
+//             } else if ( e.type === "swiperight" ) {
+//                 $( "#mypanel" ).panel( "open" );
+//             }
+//         }
+//     });
+// });
 
 
 
